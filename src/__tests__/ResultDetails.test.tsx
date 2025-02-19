8a64b674dc7e92fdb2956b69f12aa03163b87cf8
SlowpokeVG <screamer.jr@gmail.com>
2025-02-19 22:57:25 +0400

    refactor: rewrite resultdetails.test for rtk query

--- src/__tests__/ResultDetails.test.tsx
+++ src/__tests__/ResultDetails.test.tsx
@@ -1,125 +1,61 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
-import { vi, describe, it, expect } from 'vitest';
-import { Routes, Route, MemoryRouter } from 'react-router-dom';
-import ResultDetails from '../components/ResultDetails';
-import * as api from '../scripts/api';
-import { mockGifs } from './mockData';
-import '@testing-library/jest-dom';
+import { describe, expect, it, Mock, vi } from 'vitest';
+import { renderHook } from '@testing-library/react';
+import { useDetailQuery } from '../redux/api';
 
-vi.mock('../scripts/api');
-const mockGif = mockGifs[0];
+vi.mock('../redux/api', () => ({
+  useDetailQuery: vi.fn(),
+}));
 
-vi.mock('react-router-dom', async () => {
-  const actual = await vi.importActual('react-router-dom');
-  return {
-    ...actual,
-    useSearchParams: vi.fn(() => {
-      const params = new URLSearchParams('?details=CjmvTCZf2U3p09Cn0h');
-      return [
-        params,
-        vi.fn(() => {
-          params.delete('details');
-        }),
-      ];
-    }),
-  };
-});
+describe('useDetailQuery Hook', () => {
+  it('should return loading state', () => {
+    (useDetailQuery as Mock).mockReturnValue({
+      data: undefined,
+      error: undefined,
+      isLoading: true,
+    });
 
-describe('ResultDetails Component', () => {
-  it('should display a loading indicator while fetching data', async () => {
-    vi.spyOn(api, 'detail').mockImplementation(() => new Promise(() => {}));
-
-    render(
-      <MemoryRouter>
-        <Routes>
-          <Route path="/" element={<ResultDetails />} />
-        </Routes>
-      </MemoryRouter>
+    const { result } = renderHook(() =>
+      useDetailQuery({ id: '123' }, { skip: false })
     );
 
-    expect(screen.getByAltText('Spinner')).toBeInTheDocument();
+    expect(result.current.isLoading).toBe(true);
+    expect(result.current.data).toBeUndefined();
+    expect(result.current.error).toBeUndefined();
   });
 
-  it('should correctly display detailed card data when fetched', async () => {
-    vi.spyOn(api, 'detail').mockResolvedValueOnce({
-      success: true,
-      data: {
-        data: mockGif,
-        meta: {
-          status: 0,
-          msg: '',
-          response_id: '',
-        },
-      },
+  it('should return data when request is successful', () => {
+    const mockData = { id: '123', title: 'Test GIF' };
+
+    (useDetailQuery as Mock).mockReturnValue({
+      data: mockData,
+      error: undefined,
+      isLoading: false,
     });
 
-    render(
-      <MemoryRouter>
-        <Routes>
-          <Route path="/" element={<ResultDetails />} />
-        </Routes>
-      </MemoryRouter>
+    const { result } = renderHook(() =>
+      useDetailQuery({ id: '123' }, { skip: false })
     );
 
-    await waitFor(() => {
-      expect(screen.getByText(mockGif.title)).toBeInTheDocument();
-      expect(screen.getByText(`Type: ${mockGif.type}`)).toBeInTheDocument();
-      expect(
-        screen.getByText(`Uploaded on: ${mockGif.import_datetime}`)
-      ).toBeInTheDocument();
-      expect(screen.getByText('Source')).toBeInTheDocument();
-    });
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.data).toEqual(mockData);
+    expect(result.current.error).toBeUndefined();
   });
-  it('no Gif error', async () => {
-    vi.spyOn(api, 'detail').mockResolvedValueOnce({
-      success: false,
+
+  it('should return an error when request fails', () => {
+    const mockError = { message: 'Failed to fetch' };
+
+    (useDetailQuery as Mock).mockReturnValue({
+      data: undefined,
+      error: mockError,
+      isLoading: false,
     });
 
-    render(
-      <MemoryRouter>
-        <Routes>
-          <Route path="/" element={<ResultDetails />} />
-        </Routes>
-      </MemoryRouter>
+    const { result } = renderHook(() =>
+      useDetailQuery({ id: '123' }, { skip: false })
     );
 
-    await waitFor(() => {
-      expect(
-        screen.getByText('Error: Failed to load GIFs.')
-      ).toBeInTheDocument();
-    });
-  });
-  it('should close the component when the close button is clicked', async () => {
-    const mockGif = mockGifs[0];
-
-    vi.spyOn(api, 'detail').mockResolvedValueOnce({
-      success: true,
-      data: {
-        data: mockGif,
-        meta: {
-          status: 0,
-          msg: '',
-          response_id: '',
-        },
-      },
-    });
-
-    render(
-      <MemoryRouter initialEntries={['/?details=CjmvTCZf2U3p09Cn0h']}>
-        <Routes>
-          <Route path="/" element={<ResultDetails />} />
-        </Routes>
-      </MemoryRouter>
-    );
-
-    await screen.findByText('Source');
-    const closeButton = document.querySelector('.close-detail');
-    if (closeButton) {
-      fireEvent.click(closeButton);
-    }
-
-    await waitFor(() => {
-      expect(screen.queryByText('Source')).toBeNull();
-    });
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.data).toBeUndefined();
+    expect(result.current.error).toEqual(mockError);
   });
 });
