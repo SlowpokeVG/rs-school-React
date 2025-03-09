13ebe729312a341fba4f8c8ab063518978af32a6
SlowpokeVG <screamer.jr@gmail.com>
2025-03-10 01:01:39 +0400

    fix: fix eslint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,18 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "**/__tests__/**",
+      "**/__mocks__/**",
+      "**/*.test.*",
+      "**/*.spec.*",
+      "**/.next/**",
+      "**/node_modules/**",
+      "**/coverage/**",
+    ],
+  }
+  
 ];
 
 export default eslintConfig;
